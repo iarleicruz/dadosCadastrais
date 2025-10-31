b093e35799746b489e96b248a9d2492f7175b643
iarleicruz <65626531+iarleicruz@users.noreply.github.com>
2025-10-31 15:43:45 -0300

    Add files via upload

---
+++ src/pages/Form.jsx
@@ -0,0 +1,73 @@
+ 
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+function Form(){
+    
+  const [campos, setCampos] = useState({
+    txtNome: '',
+    txtIdade: 0,
+    cmbUF: '0'
+})
+ 
+function handleInputChange(event) {
+    campos[event.target.name] = event.target.value;
+    setCampos(campos);
+}
+
+   const [estados, setEstados] = useState([]);
+ 
+    useEffect(() => {
+        axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response => {
+            setEstados(response.data);
+        })
+    }, [])
+
+//     function handleFormSubmit(event){
+//     event.preventDefault();
+//     console.log(campos);
+// }
+
+    function handleFormSubmit(event){
+    event.preventDefault();
+    axios.post('http://localhost:3000/cadastro', campos).then(response => {
+        alert(response.data.dados.length + ' cadastros!');
+    })
+}
+    return (
+        <div>
+           
+            <form onSubmit={handleFormSubmit}>
+                <fieldset>
+                    <legend>
+                        <h2>Dados de Cadastro</h2>
+                    </legend>
+ 
+                    <div>
+                        <label>Nome:
+                            <input type="text" name="txtNome" id="txtNome" onChange={handleInputChange}/>
+                        </label>
+                    </div>
+ 
+                    <div>
+                        <label>Idade:
+                            <input type="number" name="txtIdade" id="txtIdade"  onChange={handleInputChange}/>
+                        </label>
+                    </div>
+ 
+                    <div>
+                        <label>UF:
+                            <select name="cmbUF" id="cmbUF" onChange={handleInputChange}>
+                                <option value="0">Selecione uma opção</option>
+                                {estados.map(estado => (<option key={estado.sigla} value={estado.sigla}>{estado.sigla}</option>))}
+                            </select>
+                        </label>
+                    </div>o
+                    <input type="submit" value="Salvar" />
+                </fieldset>
+            </form>
+        </div>
+    )
+
+}
+ 
+export default Form;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+
+import {Link} from 'react-router-dom';
+ 
+import '../App.css'
+
+function Home() {
+
+
+  return (
+ 
+      <div>
+        
+<Link to="/cadastro">Acessar cadastro</Link>
+      </div>
+   
+  )
+}
+
+export default Home
